fbbfa3f9bf1d6ba5d3a068d9ae6b9419a757e6c2
reynaldi goh <wiyogoreynaldiardianto@gmail.com>
2026-08-28 01:49:06 +0800

    add context to be able to allow usage of the data across all the pages

---
+++ src/context/CareerContext.js
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from 'react';
+
+const CareerContext = createContext(null);
+
+export function CareerProvider({ children }) {
+  const [goal, setGoal] = useState('');
+  const [stages, setStages] = useState([]);
+  const [checkedByStage, setCheckedByStage] = useState({}); // { [stageIndex]: { [todoIndex]: bool } }
+
+  const toggleTodo = (stageIndex, todoIndex) => {
+    setCheckedByStage((prev) => ({
+      ...prev,
+      [stageIndex]: {
+        ...prev[stageIndex],
+        [todoIndex]: !prev[stageIndex]?.[todoIndex],
+      },
+    }));
+  };
+
+  const clearRoadmap = () => {
+    setGoal('');
+    setStages([]);
+    setCheckedByStage({});
+  };
+
+  return (
+    <CareerContext.Provider
+      value={{ goal, setGoal, stages, setStages, checkedByStage, toggleTodo, clearRoadmap }}
+    >
+      {children}
+    </CareerContext.Provider>
+  );
+}
+
+export function useCareer() {
+  const ctx = useContext(CareerContext);
+  if (!ctx) throw new Error('useCareer must be used within a CareerProvider');
+  return ctx;
+}
